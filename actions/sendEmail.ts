92c8ce9aa30f7a94efe9ef6b5ff9196aac3eeec3
musa-surmovi <167339409+musa-surmovi@users.noreply.github.com>
2024-12-17 13:26:19 +0500

    first commit 1

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   // try {
   //   data = await resend.emails.send({
   //     from: "Contact Form <onboarding@resend.dev>",
-  //     to: "mxbilal0@gmail.com",
+  //     to: "musa.surmovi@gmail.com",
   //     subject: "Message from contact form",
   //     reply_to: senderEmail,
   //     react: React.createElement(ContactFormEmail, {
