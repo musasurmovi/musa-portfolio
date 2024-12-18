b1449dfa78c4839d5041f0bceb27078a1dc24e46
musa-surmovi <167339409+musa-surmovi@users.noreply.github.com>
2024-12-18 13:06:17 +0500

    first commit 8

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
@@ -8,17 +8,29 @@ import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
+  const [showAll, setShowAll] = useState(false);
+
+  const projectsToShow = showAll ? projectsData : projectsData.slice(0, 2);
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
-        {projectsData.map((project, index) => (
+        {projectsToShow.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
           </React.Fragment>
         ))}
       </div>
+
+      {!showAll && (
+     <div className="w-full text-center">  <button
+     onClick={() => setShowAll(true)}
+     className="mt-10 px-6 py-1.5 border text-white border-blue-700 rounded-lg text-base bg-blue-500 hover:bg-blue-600   font-medium "
+   >
+     Show all
+   </button></div>
+      )}
     </section>
   );
 }
